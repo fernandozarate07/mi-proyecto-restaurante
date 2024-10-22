03da6ecaf300ad670c3d812bfc78f7a0b0f89963
Fernando Zárate <150300719+fernandozarate07@users.noreply.github.com>
2024-10-22 19:14:39 -0300

    Create src directory with initial files, install Webpack and loaders
    - Created the src directory to organize project files.
    - Added initial files: index.js, template.html, and style.css.
    - Installed Webpack and configured it for module bundling.
    - Installed file-loader to manage image assets.

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i, 
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+              outputPath: 'images/',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  mode: 'development',
+};
